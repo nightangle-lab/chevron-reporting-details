3b474df00c8bd161a970467e63ca7c7f7c679476
nightangle-lab <152067680+nightangle-lab@users.noreply.github.com>
2023-12-03 15:30:09 +0600

    Update main.js

--- main.js
+++ main.js
@@ -4,6 +4,37 @@
   console.log(status);
   console.log(dev);
 }
+   // Replace 'your-file.csv' with the path to your CSV file
+    Papa.parse('data.csv', {
+      download: true,
+      header: true,
+      dynamicTyping: true,
+      complete: function (results) {
+        displayData(results.data);
+      }
+    });
+
+    function displayData(data) {
+      var table = document.getElementById('csvData');
+      var headers = Object.keys(data[0]);
+
+      // Create header row
+      var headerRow = table.insertRow(0);
+      headers.forEach(function (header) {
+        var th = document.createElement('th');
+        th.textContent = header;
+        headerRow.appendChild(th);
+      });
+
+      // Create data rows
+      data.forEach(function (row) {
+        var tr = table.insertRow();
+        headers.forEach(function (header) {
+          var td = tr.insertCell();
+          td.textContent = row[header];
+        });
+      });
+    }
  function performSearch() {
         var input, filter, table, tr, td, i, txtValue;
         input = document.getElementById("searchInput");
